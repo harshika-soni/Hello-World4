48e38a23273c17ac7e4ca4ac0b8e0a106b7e8821
harshika-soni <harshikamuz@gmail.com>
2024-10-16 12:10:46 -0700

    1

---
+++ Project/src/test/java/campaign/Sampletest.java
@@ -0,0 +1,15 @@
+package campaign;
+
+import org.testng.annotations.Test;
+
+public class Sampletest {
+	@Test
+	public void m1() {
+		System.out.println("m1 is printed");
+	}
+	@Test
+	public void m2() {
+		System.out.println("m2 is printed");
+	}
+
+}
